989501d550a986064bdb3bc463eb01b7a3920a90
Victor Magalhaes <victormagalhaesfaria@hotmail.com>
2020-01-23 21:29:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path')
+
+const exphbs = require('express-handlebars')
+
+app.set('views',path.join(__dirname,"src", 'app', 'views'))
+
+app.engine('.hbs', exphbs(
+  { extname: '.hbs',
+    layoutsDir: path.join('src', 'app', 'views', 'layouts'),
+    partialsDir: path.join('src', 'app', 'views', 'partials') 
+  })
+)
+
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res){
+  res.render('chat', {})
+});
+
+const users = []
+
+io.on('connection', function(socket){
+
+  socket.on('newUser', user => {
+    users.push(user)
+    io.emit('usersUpdated', users)
+  })
+
+  socket.on('chat message', ({id: userId, message: msg}) => {
+
+    if(msg.includes('|')){
+      const msgArray = msg.split('|');
+      const targetedUsers = []
+      const socketIds = []
+
+      msg = msgArray[0]
+
+      const usersArray= msgArray[1].split(';')
+      for (let index = 0; index < usersArray.length; index++) {
+        targetedUsers.push(usersArray[index].trim());
+      }
+
+      for(const u of targetedUsers){
+        let [{id}] = users.filter(user => user.name === u)
+        socketIds.push(id)
+      }
+
+      socketIds.push(userId)
+
+      for(const targetId of socketIds){
+        console.log(`Mandando mensagem privada para: ${targetId}`)
+        io.to(targetId).emit('chat message', msg)
+      }
+
+      
+    }
+    else io.emit('chat message', msg);
+  });
+    
+});
+
+
+ 
+// inicia o servidor na porta informada, no caso vamo iniciar na porta 3000
+http.listen(3000, function(){
+  console.log('Servidor rodando em: http://localhost:3000');
+});
